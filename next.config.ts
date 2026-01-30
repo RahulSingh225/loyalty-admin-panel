6ddfce97607c5bc8bc9da964fd830c4b53719a01
Rahul Singh <42008021+RahulSingh225@users.noreply.github.com>
2026-01-30 16:21:07 +0530

    Configure remote image patterns in next.config.ts
    Add remote image patterns for S3 bucket

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sturlite-uat.s3.ap-south-1.amazonaws.com', // Replace with your S3 hostname
+        pathname: '/**', // Allows images from any path in the bucket
+      },
+    ],
+  },
   output: 'standalone',
   /* config options here */
 };
